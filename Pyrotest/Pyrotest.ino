bf083227ea4b280ace2d2db8e6842368ee4866aa
Jordan Layman <jordanmlayman@gmail.com>
2013-08-22 02:09:47 -0700

    Shift Chain Split (Incomplete)
    Committing for code pull

--- Pyrotest/Pyrotest.ino
+++ Pyrotest/Pyrotest.ino
@@ -1,13 +1,19 @@
 #include <avr/pgmspace.h>
 #include <Shifter.h>
 
+#define CHAIN_MODE 2
 
 #define SER_Pin 7 //SER_IN
 #define RCLK_Pin 2 //L_CLOCK
 #define SRCLK_Pin 6 //CLOCK
 
+#define SER_Pin2 10 //SER_IN
+#define RCLK_Pin2 8 //L_CLOCK
+#define SRCLK_Pin2 9 //CLOCK
+
 #define NUM_REGISTERS 12 //how many registers are in the chain
 #define TOTAL_NODES 91
+#define CHAIN1 47
 
 uint32_t sustain = 50;
 uint32_t frequency = 100;
@@ -15,15 +21,20 @@ uint32_t frequency = 100;
 // const prog_int8_t mappingArray_P[TOTAL_NODES] PROGMEM = { 31, 74, 29, 90, 37, 33, 44, 34, 67, 76, 80, 30, 93, 75, 20, 35, 21, 65, 41, 42, 59, 69, 58, 85, 52, 53, 95, 89, 79, 39, 19, 87, 8, 22, 46, 32, 70, 66, 61, 18, 36, 86, 83, 77, 73, 84, 28, 94, 9, 11, 55, 68, 57, 63, 60, 26, 38, 43, 15, 12, 91, 72, 88, 16, 92, 64, 47, 51, 81, 71, 62, 50, 49, 27, 78, 10, 17, 14, 13, 82, 40, 45, 54, 56, 23, 48, 0, 1, 2, 3, 4, 5, 6, 7, 24, 25 };
 // const uint8_t map[TOTAL_NODES] = [31, 74, 29, 90, 37, 33, 44, 34, 67, 76, 80, 30, 93, 75, 20, 35, 21, 65, 41, 42, 59, 69, 58, 85, 52, 53, 95, 89, 79, 39, 19, 87, 8, 22, 46, 32, 70, 66, 61, 18, 36, 86, 83, 77, 73, 84, 28, 94, 9, 11, 55, 68, 57, 63, 60, 26, 38, 43, 15, 12, 91, 72, 88, 16, 92, 64, 47, 51, 81, 71, 62, 50, 49, 27, 78, 10, 17, 14, 13, 82, 40, 45, 54, 56, 23, 48, 0, 1, 2, 3, 4, 5, 6, 7, 24, 25];
 
-
 //initaize shifter using the Shifter library
-Shifter shifter(SER_Pin, RCLK_Pin, SRCLK_Pin, NUM_REGISTERS); 
+Shifter shifter1(SER_Pin, RCLK_Pin, SRCLK_Pin, CHAIN1);
+Shifter shifter2(SER_Pin2, RCLK_Pin2, SRCLK_Pin2, NUM_REGISTERS-CHAIN1); 
+
+SoftwareSerial mySerial(10, 11); // RX, TX
 
 void setup(){
-	shifter.clear();
-	shifter.write();
+	shifter1.clear();
+	shifter2.clear();
+	
+	shifter1.write();
+	shifter2.write();
 
-  Serial.begin(4800);
+  Serial.begin(115200);
 
 	// pepRally(); // Fire down and Fire up
  	delay(1000);
@@ -170,11 +181,11 @@ void fireAll() {
 void fireAll(uint32_t sust) {
 	Serial.print("Firing All");
 	
-	shifter.setAll(HIGH);
-  shifter.write(); //send changes to the chain and display them  
+	shifter1.setAll(HIGH);
+  shifter1.write(); //send changes to the chain and display them  
   delay(sust);
-  shifter.clear(); //set all pins on the shift register chain to LOW
-  shifter.write(); //send changes to the chain and display them  
+  shifter1.clear(); //set all pins on the shift register chain to LOW
+  shifter1.write(); //send changes to the chain and display them  
 		
 }
 
@@ -185,8 +196,8 @@ void fire(uint16_t pin) {
 
 void fire(uint16_t pin, uint32_t sust) {
 	
-	Serial.print("Firing ");
-  Serial.println(pin);
+	// Serial.print("Firing ");
+	//   Serial.println(pin);
   
 	// shifter.setPin(pin, HIGH);
 	//   shifter.write(); //send changes to the chain and display them  
@@ -203,19 +214,41 @@ void fire(uint16_t pin, uint32_t sust) {
 void prime(uint16_t n) {	
 	// Serial.print("Priming ");
 	//   Serial.println(n);
+
+		if (CHAIN_MODE == 2 && n >= CHAIN1) {
+			
+			Serial.print("Firing Chain 2: ");
+		  Serial.println(n-CHAIN1);
+			
+			
+			shifter2.setPin(n-CHAIN1, HIGH);
+		} else {
+			
+			Serial.print("Firing Chain 1: ");
+		  Serial.println(n);			
+			
+			shifter1.setPin(n, HIGH);
+		}
 	
-	shifter.setPin(n, HIGH);	
+	
 }
 
-void ignite(uint32_t sust) {
-	// Serial.println("Igniting! ");
+void ignite(uint32_t sust) { 		
+										
+										// Serial.println("Igniting! ");
 		
-	shifter.write(); //send changes to the chain and display them  
+	shifter1.write(); //send changes to the chain and display them  
+	shifter2.write(); //send changes to the chain and display them  	
   delay(sust);
-  shifter.clear(); //send changes to the chain and display them  
-	shifter.write();
+  shifter1.clear(); //send changes to the chain and display them  
+  shifter2.clear(); //send changes to the chain and display them  
+	shifter1.write();
+	shifter2.write(); //send changes to the chain and display them  	
+	
+	
 }
 void kill(uint16_t n) {
-	shifter.setPin(n, LOW);
-	shifter.write();
+/// FIX FOR 2
+	shifter1.setPin(n, LOW);
+	shifter1.write();
 }
